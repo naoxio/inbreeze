7613b789e8dd6c366f6a4e500d6e62f23fbc8339
waotzi <waotzi@proton.me>
2023-06-20 14:01:46 +0700

    add secondary menu button and backbutton

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -15,6 +17,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Inner Breeze',
         home: MyHomePage(),
         theme: ThemeData.from(
@@ -45,6 +48,13 @@ class MyAppState extends ChangeNotifier {
     notifyListeners();
   }
 
+  var selectedIndex = 0;
+
+  void changeIndex(int i) {
+    selectedIndex = i;
+    notifyListeners();
+  }
+
   void toggleFavorite() {
     if (favorites.contains(current)) {
       favorites.remove(current);
@@ -61,15 +71,194 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    Widget pageToDisplay;
+
+    // Conditionally set the page to display based on the selectedIndex using switch-case
+    switch (appState.selectedIndex) {
+      case 0:
+        pageToDisplay = MainPage();
+        break;
+      case 1:
+        pageToDisplay = TimestopPage();
+        break;
+      default:
+        pageToDisplay =
+            MainPage(); // Set a default page in case selectedIndex is not handled
+    }
+
     return LayoutBuilder(
       builder: (context, constrains) {
         return Scaffold(
-          body: MainPage(),
+          body:
+              pageToDisplay, // Render the appropriate page based on the selectedIndex
         );
       },
+    );
+  }
+}
+
+class CustomTimerPainter extends CustomPainter {
+  final Animation<double> animation;
+  final Color backgroundColor;
+  final Color color;
+
+  CustomTimerPainter({
+    required this.animation,
+    this.backgroundColor = Colors.red,
+    this.color = Colors.black,
+  }) : super(repaint: animation);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = backgroundColor
+      ..strokeWidth = 10.0
+      ..strokeCap = StrokeCap.butt
+      ..style = PaintingStyle.stroke;
+
+    canvas.drawCircle(size.center(Offset.zero), size.width / 2.0, paint);
+    paint.color = color;
+    double progress = (1.0 - animation.value) * 2 * math.pi;
+    canvas.drawArc(Offset.zero & size, math.pi * 1.5, -progress, false, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomTimerPainter oldDelegate) {
+    return animation.value != oldDelegate.animation.value ||
+        color != oldDelegate.color ||
+        backgroundColor != oldDelegate.backgroundColor;
+  }
+}
+
+class CountDownTimer extends StatefulWidget {
+  @override
+  _CountDownTimerState createState() => _CountDownTimerState();
+}
+
+class _CountDownTimerState extends State<CountDownTimer>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  String get timerString {
+    Duration duration = controller.duration! * controller.value;
+    return '${duration.inMinutes}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 50),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    ThemeData themeData = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(
+          onPressed: () => {appState.changeIndex(0)},
+        ),
+      ),
+      body: AnimatedBuilder(
+          animation: controller,
+          builder: (context, child) {
+            return Stack(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Expanded(
+                        child: Align(
+                          alignment: FractionalOffset.center,
+                          child: AspectRatio(
+                            aspectRatio: 1.0,
+                            child: Stack(
+                              children: <Widget>[
+                                Positioned.fill(
+                                  child: CustomPaint(
+                                    painter: CustomTimerPainter(
+                                      animation: controller,
+                                      backgroundColor: Colors.white,
+                                      color: themeData.indicatorColor,
+                                    ),
+                                  ),
+                                ),
+                                Align(
+                                  alignment: FractionalOffset.center,
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      Text(
+                                        "Count Down Timer",
+                                        style: TextStyle(
+                                            fontSize: 20.0,
+                                            color: Colors.white),
+                                      ),
+                                      Text(
+                                        timerString,
+                                        style: TextStyle(
+                                            fontSize: 112.0,
+                                            color: Colors.white),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      AnimatedBuilder(
+                          animation: controller,
+                          builder: (context, child) {
+                            return FloatingActionButton.extended(
+                                onPressed: () {
+                                  if (controller.isAnimating)
+                                    controller.stop();
+                                  else {
+                                    controller.reverse(
+                                        from: controller.value == 0.0
+                                            ? 1.0
+                                            : controller.value);
+                                  }
+                                },
+                                icon: Icon(controller.isAnimating
+                                    ? Icons.pause
+                                    : Icons.play_arrow),
+                                label: Text(
+                                    controller.isAnimating ? "Pause" : "Play"));
+                          }),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          }),
+    );
+  }
+}
+
+class TimestopPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    return DefaultTabController(
+      length: 2,
+      child: CountDownTimer(),
     );
   }
 }
@@ -92,6 +281,12 @@ class MainPage extends StatelessWidget {
               'Time to breathe'.toUpperCase(),
             ),
           ),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {},
+            ),
+          ],
           bottom: TabBar(
             tabs: [
               Tab(text: 'Today'),
@@ -109,7 +304,9 @@ class MainPage extends StatelessWidget {
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () => {},
+          onPressed: () => {
+            appState.changeIndex(1),
+          },
         ),
       ),
     );
@@ -138,38 +335,48 @@ class ProgressView extends StatelessWidget {
   }
 }
 
-class TodayView extends StatelessWidget {
+class TodayView extends StatefulWidget {
   TodayView({
     super.key,
   });
+
+  @override
+  State<TodayView> createState() => _TodayViewState();
+}
+
+class _TodayViewState extends State<TodayView> {
   var roundsCompleted = 0;
+
   var averageRoundDuration = 0;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: SizedBox(
-        width: 300,
-        child: ListView(
-          children: [
-            Text(
-              'Today'.toUpperCase(),
-              style: TextStyle(fontSize: 32),
-            ),
-            Text(
-              '$roundsCompleted rounds completed',
-              style: TextStyle(fontSize: 24.0),
-            ),
-            Text(
-              '${averageRoundDuration.toStringAsFixed(2)} average round duration',
-              style: TextStyle(fontSize: 24.0),
-            ),
-            TableCalendar(
-              firstDay: DateTime.utc(2010, 10, 16),
-              lastDay: DateTime.now(),
-              focusedDay: DateTime.now(),
-            ),
-          ],
+      child: Padding(
+        padding: const EdgeInsets.all(22.0),
+        child: SizedBox(
+          width: 300,
+          child: ListView(
+            children: [
+              Text(
+                'Today'.toUpperCase(),
+                style: TextStyle(fontSize: 32),
+              ),
+              Text(
+                '$roundsCompleted rounds completed',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              Text(
+                '${averageRoundDuration.toStringAsFixed(2)} average round duration',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              TableCalendar(
+                firstDay: DateTime.utc(2010, 10, 16),
+                lastDay: DateTime.now(),
+                focusedDay: DateTime.now(),
+              ),
+            ],
+          ),
         ),
       ),
     );
