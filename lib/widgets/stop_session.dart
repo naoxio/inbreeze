9de705ba5b06b7c5f4036fb267794c1579f1f935
Martin Schiller <waotzi@proton.me>
2023-11-24 17:56:49 +0700

    fix stop session dialog wrapping

--- lib/widgets/stop_session.dart
+++ lib/widgets/stop_session.dart
@@ -56,41 +56,42 @@ class _StopSessionButtonState extends State<StopSessionButton> {
   }
 
   void _showExitConfirmationDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Stop Session'),
-          content: Text('Are you sure you want to end the session?'),
-          actions: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                OutlinedButton(
-                  onPressed: () {
-                    _navigateToResults();
-                  },
-                  style: OutlinedButton.styleFrom(
-                    side: BorderSide(color: Colors.red),
-                  ),
-                  child: Text(
-                    'Stop Session',
-                    style: TextStyle(color: Colors.red),
-                  ),
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Stop Session'),
+        content: Text('Are you sure you want to end the session?'),
+        actions: <Widget>[
+          Wrap(
+            alignment: WrapAlignment.center,
+            spacing: 8.0,
+            children: [
+              OutlinedButton(
+                onPressed: () {
+                  _navigateToResults();
+                },
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(color: Colors.red),
                 ),
-                OutlinedButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Text('Continue Session'),
+                child: Text(
+                  'Stop Session',
+                  style: TextStyle(color: Colors.red),
                 ),
-              ],
-            ),
-          ],
-        );
-      },
-    );
-  }
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Continue Session'),
+              ),
+            ],
+          ),
+        ],
+      );
+    },
+  );
+}
 
   @override
   Widget build(BuildContext context) {
