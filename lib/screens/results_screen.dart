75be5cbfa1c373e2defb1ce30c8f440a1debafe9
Martin Schiller <waotzi@proton.me>
2023-11-24 17:54:16 +0700

    new attempt

--- lib/screens/results_screen.dart
+++ lib/screens/results_screen.dart
@@ -113,41 +113,40 @@ class _ResultsScreenState extends State<ResultsScreen> {
       bottomNavigationBar: BottomAppBar(
         color: Colors.transparent,
         child: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SwitchListTile(
-                  title: Text("Save Progress"),
-                  value: saveProgress,
-                  onChanged: (rounds > 0)
-                      ? (bool value) {
-                          setState(() {
-                            saveProgress = value;
-                          });
-                        }
-                      : null,
-                  activeColor: Colors.teal,
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  height: 52,
-                  child: TextButton(
-                    onPressed: () {
-                      _handleClose();
-                      context.go('/home');
-                    },
-                    child: Text(
-                      'Close',
-                      style: BreezeStyle.bodyBig
-                    ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SwitchListTile(
+                title: Text("Save Progress"),
+                value: saveProgress,
+                onChanged: (rounds > 0)
+                    ? (bool value) {
+                        setState(() {
+                          saveProgress = value;
+                        });
+                      }
+                    : null,
+                activeColor: Colors.teal,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 52,
+                child: TextButton(
+                  onPressed: () {
+                    _handleClose();
+                    context.go('/home');
+                  },
+                  child: Text(
+                    'Close',
+                    style: BreezeStyle.bodyBig
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
+
     );
   }
 }
